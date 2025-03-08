3d711bb3fc6cc65428c9c99d22c9be3872449d9b
sanchitb23 <dev.sanchitb23@gmail.com>
2025-03-09 03:37:36 +0530

    add table creation logic for URL storage; handle database connection errors

--- database/database.go
+++ database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/lib/pq"
 	"os"
 	"url-shortener/logger"
@@ -14,7 +15,8 @@ func InitDB() {
 	if dsn == "" {
 		logger.Log.Panic("DATABASE_URL environment variable not set")
 	}
-	DB, err := sql.Open("postgres", dsn)
+	var err error
+	DB, err = sql.Open("postgres", dsn)
 	if err != nil {
 		logger.Log.Panic("Failed to connect to the database", err)
 	}
@@ -23,4 +25,23 @@ func InitDB() {
 		logger.Log.Panic("Failed to ping the database", err)
 	}
 	logger.Log.Info("Successfully connected to the database")
+	createTables()
+}
+
+func createTables() {
+	tableName := os.Getenv("URL_TABLE_NAME")
+	createURLsTable := fmt.Sprintf(`
+				CREATE TABLE IF NOT EXISTS %s (
+					id SERIAL PRIMARY KEY,
+					long_url TEXT NOT NULL,
+					short_url TEXT NOT NULL,
+					user_id INT NOT NULL
+				)
+			`, tableName)
+	_, err := DB.Exec(createURLsTable)
+	if err != nil {
+		logger.Log.Panic("Failed to create urls table", err)
+	} else {
+		logger.Log.Info("Successfully created urls table")
+	}
 }
