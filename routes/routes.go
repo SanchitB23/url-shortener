30c7eb4e1bd879cd6ae6ec2a56828f1f701a48e9
sanchitb23 <dev.sanchitb23@gmail.com>
2025-03-08 21:04:00 +0530

    add redirect from root to health check endpoint

--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,9 @@ import (
 )
 
 func RegisterRoutes(server *gin.Engine) {
+	server.GET("/", func(context *gin.Context) {
+		context.Redirect(http.StatusMovedPermanently, "/health")
+	})
 	server.GET("/health", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"message": "URL Shortener API is running"})
 	})
